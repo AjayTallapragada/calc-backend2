ace5860d2cfd3f62c87b7b80390b5144c76c69ea
Ajay Tallapragada <110114397+AjayTallapragada@users.noreply.github.com>
2025-08-26 11:30:51 +0530

    Update ArithmeticController.java

--- src/main/java/klu/controller/ArithmeticController.java
+++ src/main/java/klu/controller/ArithmeticController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/calc")  
+
 
 public class ArithmeticController {
  @GetMapping("/add/{A}/{B}")
